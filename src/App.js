90c55060a94205b4d9b5a8c2752a45101dce0a75
Tamer Tawfik <tamertawfiklxr@gmail.com>
2022-08-13 04:18:51 +0200

    Added a tailwind to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import styles from './style';
+import { Navbar } from './components';
 
-const App = () => {
-  return (
-    <div>Hello there.</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+        {/* <Hero /> */}
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        {/* <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer /> */}
+      </div>
+    </div>
+  </div>
+);
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+const Navbar = () => {
+    return (
+        <h1>Navbar</h1>
+    )
+}
+
+export default Navbar;
