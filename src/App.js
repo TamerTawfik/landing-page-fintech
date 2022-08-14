d577321a8fc858a6e9b9bc19baf230002cb8d9f2
Tamer Tawfik <tamertawfiklxr@gmail.com>
2022-08-14 06:46:38 +0200

    Added testimonials section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styles from './style';
-import { Navbar, Hero, Stats, Business, Billing, CardDeal } from './components';
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials } from './components';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -21,8 +21,8 @@ const App = () => (
         <Business />
         <Billing />
         <CardDeal />
-        {/* <Testimonials />
-        <Clients />
+        <Testimonials />
+        {/* <Clients />
         <CTA />
         <Footer /> */}
       </div>
