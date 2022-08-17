6aa96f9a29f04d2c3aad068d22c85b7e7dca2c1a
Tamer Tawfik <tamertawfiklxr@gmail.com>
2022-08-17 23:11:38 +0200

    Added scroll animation

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,6 +1,7 @@
 import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const FeatureCard = ({ icon, title, content, index }) => (
     <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
@@ -20,25 +21,28 @@ const FeatureCard = ({ icon, title, content, index }) => (
 
 const Business = () => (
     <section id="features" className={layout.section}>
-        <div className={layout.sectionInfo}>
-            <h2 className={styles.heading2}>
-                You do the business, <br className="sm:block hidden" /> we’ll handle
+        <AnimationOnScroll animateIn="animate__fadeInLeft" duration={3}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>
+                    You do the business, <br className="sm:block hidden" /> we’ll handle
         the money.
       </h2>
-            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-                With the right credit card, you can improve your financial life by
-                building credit, earning rewards and saving money. But with hundreds
-                of credit cards on the market.
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    With the right credit card, you can improve your financial life by
+                    building credit, earning rewards and saving money. But with hundreds
+                    of credit cards on the market.
       </p>
 
-            <Button styles={`mt-10`} />
-        </div>
-
-        <div className={`${layout.sectionImg} flex-col`}>
-            {features.map((feature, index) => (
-                <FeatureCard key={feature.id} {...feature} index={index} />
-            ))}
-        </div>
+                <Button styles={`mt-10`} />
+            </div>
+        </AnimationOnScroll>
+        <AnimationOnScroll animateIn="animate__fadeInRight" duration={3}>
+            <div className={`${layout.sectionImg} flex-col`}>
+                {features.map((feature, index) => (
+                    <FeatureCard key={feature.id} {...feature} index={index} />
+                ))}
+            </div>
+        </AnimationOnScroll>
     </section>
 );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
                 {navLinks.map((nav, index) => (
                     <li
                         key={nav.id}
-                        className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                        className={`font-poppins font-normal cursor-pointer hover:text-secondary text-[16px] ${
                             active === nav.title ? "text-white" : "text-dimWhite"
                             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
                         onClick={() => setActive(nav.title)}
@@ -42,7 +42,7 @@ const Navbar = () => {
                         {navLinks.map((nav, index) => (
                             <li
                                 key={nav.id}
-                                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                                className={`font-poppins font-medium cursor-pointer hover:text-secondary text-[16px] ${
                                     active === nav.title ? "text-white" : "text-dimWhite"
                                     } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                                 onClick={() => setActive(nav.title)}
